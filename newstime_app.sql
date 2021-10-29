d87b9030df837d31e31a8cd6d8244c74286ffbd2
cristian.lucumi00 <cristian.lucumi00usc.edu.co>
2021-10-29 11:58:51 -0500

    grouping 3 things: DB, Frontend, and ApiRest

---
+++ newstime_app.sql
@@ -0,0 +1,23 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         Microsoft SQL Server 2019 (RTM-GDR) (KB4583458) - 15.0.2080.9
+-- SO del servidor:              Windows 10 Pro 10.0 <X64> (Build 19043: )
+-- HeidiSQL Versión:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES  */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- La exportación de datos fue deseleccionada.
+
+-- La exportación de datos fue deseleccionada.
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
